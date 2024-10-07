5636b4e0bf51fca066ffcc9ffef2418aa9be8417
ghost712xsa <149754064+ghost712xsa@users.noreply.github.com>
2024-10-07 13:56:30 -0900

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenvolvimento de Software',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação amplamente utilizada para desenvolvimento web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'A capital do Japão é Tóquio.'
+)
+
+criaCartao(
+    'Desenvolvimento de Software',
+    'O que é um método?',
+    'Um método é uma função que está associada a um objeto ou classe.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se cumprimenta alguém em inglês?',
+    'Para cumprimentar alguém em inglês, dizemos "Hello" (relacionado a "olá").'
+)
